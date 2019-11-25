92ee97d8fb49053b6c46b1456a571d609759c936
mitsuru793 <mitsuru793@gmail.com>
2019-11-26 03:02:40 +0900

    remove unneeded code

--- src/Application/Actions/Game/ViewGameAction.php
+++ src/Application/Actions/Game/ViewGameAction.php
@@ -3,18 +3,10 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\Game;
 
-use App\Domain\Game\GameRepository;
 use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Log\LoggerInterface;
-use Slim\Views\Twig;
 
 final class ViewGameAction extends GameAction
 {
-    public function __construct(LoggerInterface $logger, Twig $view, GameRepository $gameRepository)
-    {
-        parent::__construct($logger, $view, $gameRepository);
-    }
-
     protected function action(): Response
     {
         $gameId = (int)$this->resolveArg('id');
